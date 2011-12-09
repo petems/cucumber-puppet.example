bf2ee9843645972b0a3d148329c68772edd775fb
Nikolay Sturm <sturm@erisiandiscord.de>
2011-12-09 17:34:18 +0100

    configure cucumber-puppet

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,7 +1,7 @@
 Before do
   # adjust local configuration like this
-  # @puppetcfg['confdir']  = File.join(File.dirname(__FILE__), '..', '..')
-  # @puppetcfg['manifest'] = File.join(@puppetcfg['confdir'], 'manifests', 'site.pp')
+  @puppetcfg['confdir']  = File.join(File.dirname(__FILE__), '..', '..')
+  @puppetcfg['manifest'] = File.join(@puppetcfg['confdir'], 'manifests', 'site.pp')
   # @puppetcfg['modulepath']  = "/srv/puppet/modules:/srv/puppet/site-modules"
 
   # adjust facts like this
