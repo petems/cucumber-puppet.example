0fe0348b4c95099fc181610efad671c00529d2b9
Nikolay Sturm <sturm@erisiandiscord.de>
2011-12-09 18:12:41 +0100

    implement the step, writing the code I wish I had
    the policy still fails, due to the step implementation using methods are
    not yet written
    
    the goal here is, to have simple, high level step implementations and
    write small helper methods for doing the real work

--- features/steps/catalog_policy.rb
+++ features/steps/catalog_policy.rb
@@ -3,3 +3,9 @@ Given /^a node with role "([^\"]*)"$/ do |role|
   # (in manifests/templates.pp)
   @klass = role
 end
+
+Then /^puppet should ensure all packages are up\-to\-date$/ do
+  packages.each do |package|
+    package.should be_latest
+  end
+end
