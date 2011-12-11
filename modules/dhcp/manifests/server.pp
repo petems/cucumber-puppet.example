5b2ea1334a58ccf5b8ecb65dfbddef64be0db97e
Nikolay Sturm <sturm@erisiandiscord.de>
2011-12-11 12:27:01 +0100

    install config file
    My goal here is to configure the dhcp server for my network and add
    static entries for some nodes. This will serve as an example of how to
    test custom functions with cucumber-puppet.
    
    Running 'rake cucumber-puppet' will fail at this point, as the template
    is missing.

--- modules/dhcp/manifests/server.pp
+++ modules/dhcp/manifests/server.pp
@@ -2,4 +2,9 @@ class dhcp::server {
   package { 'isc-dhcp-server':
     ensure => latest
   }
+
+  file { '/etc/dhcp/dhcpd.conf':
+    content => template('dhcp/dhcpd.conf.erb'),
+    require => Package['isc-dhcp-server']
+  }
 }
