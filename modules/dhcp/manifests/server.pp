de525a0379e971fafb664f06065599f8e45d2add
Nikolay Sturm <sturm@erisiandiscord.de>
2011-12-11 18:33:56 +0100

    template refactoring: step 2
    add missing $hosts variable and call a custom function to read the host
    data from a file
    -> write the code I wish I had, tests still red

--- modules/dhcp/manifests/server.pp
+++ modules/dhcp/manifests/server.pp
@@ -3,6 +3,7 @@ class dhcp::server($host_list = "data/hosts") {
     ensure => latest
   }
 
+  $hosts = dhcp_clients($host_list)
   file { '/etc/dhcp/dhcpd.conf':
     content => template('dhcp/dhcpd.conf.erb'),
     require => Package['isc-dhcp-server']
